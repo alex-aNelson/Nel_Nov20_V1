1866fbe82beae574ef2b4d9fb7065244f7224045
alex-aNelson <159586652+alex-aNelson@users.noreply.github.com>
2024-11-20 13:44:42 -0600

    added student class

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Nel_Nov20.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string? StudentFirstName { get; set; }
+        public string? StudentLastName { get; set; }
+
+    }
+}
